c35220e58a80af120b58d484b6066d8c70a4a5b4
Omer <devops.omer@gmail.com>
2025-06-26 15:11:26 +0530

    updating practice.sh

--- practice.sh
+++ practice.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-> Cloning the shell repo to my account and adding a branch and creating a PR
+> Cloning the shell repo to my account and adding a branch and creating a PR request
+
+> Later will be deleting the branch and commit mades
